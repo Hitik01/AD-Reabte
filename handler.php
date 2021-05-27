443d2121b50097c67b3ebf0e5b95d7b11b9ee98a
Hitik001 <hitik456mall@gmail.com>
2021-05-27 10:35:37 +0530

    registration updated

--- handler.php
+++ handler.php
@@ -1,26 +1,31 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['name']; // Get Name value from HTML Form
+        $email_id = $_POST['email']; // Get Email Value
+        $mobile_no = $_POST['mobile']; // Get Mobile No
+        $msg = $_POST['message']; // Get Message Value
+        $to = "hitik456mall@gmail.com"; // You can change here your Email
+        $subject = "'$name' has been sent a mail"; // This is your subject
+		$headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        // More headers
+        $headers .= 'From: Admin <admin@websapex.com>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
+        $headers .= 'Cc: info@websapex.com' . "\r\n"; // If you want add cc
+        $headers .= 'Bcc: boss@websapex.com' . "\r\n"; // If you want add Bcc
 
- */
-require_once './vendor/autoload.php';
+        if(mail($to,$subject,$message,$headers)){
+            // Message if mail has been sent
+            echo "<script>
+                    alert('Mail has been sent Successfully.');
+                </script>";
+        }
 
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler(); 
-
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('comments')->maxLength(6000);
-
-
-
-
-$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
-
-echo $pp->process($_POST);
+        else{
+            // Message if mail has been not sent
+            echo "<script>
+                    alert('EMAIL FAILED');
+                </script>";
+        }
+    }
+?>
