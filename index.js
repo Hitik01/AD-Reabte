7949d241a07dc1b71e7f95abfb640275feb8bec0
Hitik001 <hitik456mall@gmail.com>
2021-05-31 15:59:53 +0530

    post

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 
 
 $(document).ready(function(){
+  $('#postBtn')
+  .click(async (e) => {
+    e.preventDefault()
+    console.log('hello')
+    axios.post('https://adrebate.herokuapp.com/api/submitform', {
+    Name:"xxxx",Email:"xxxx",Phone:"xxxx",Message:"xxxx",Time:new Date().getMilliseconds()
+    })
+    .then(function (response) {
+      console.log('response');
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  })
 
     //hamburger Toggle
     $('.hamburger').click(function(event){
@@ -15,5 +29,5 @@ $(document).ready(function(){
 });
 
 });
-
+ 
 
